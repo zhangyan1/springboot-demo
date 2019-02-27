b198bb08dd9db18fd10b229143d7f0d5307982a6
zhangyan <578033593@qq.com>
2019-02-27 19:50:22 +0800

    fix

---
+++ src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/java/web/application/Application.java
@@ -0,0 +1,34 @@
+#set( $symbol_pound = '#')
+#set( $symbol_dollar = '$')
+#set( $symbol_escape = '\')
+
+package ${package}.web.application;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * Created by ${userName} on ${today}.
+ */
+
+@SpringBootApplication(scanBasePackages = {
+        "${gourpId}.web",
+        "${gourpId}.core",
+})
+@MapperScan(basePackages = {"${{gourpId}}.dal.mapper"})
+public class Application extends SpringBootServletInitializer {
+
+    private static final Class<Application> application = Application.class;
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.application);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(application, args);
+    }
+
+}
